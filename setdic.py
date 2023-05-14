d98ee539e0d5fea4108213cdeca97a1ba2970beb
dharshigo <dharshigo@gmail.com>
2023-05-14 13:06:26 +0530

    add print

---
+++ setdic.py
@@ -0,0 +1,17 @@
+#dictionary with for loop
+x={'name':'kamal','age': 12}
+for k,v in x.items():
+    print(k,v)
+#set
+class1={'amal','kamal','nimal','sunil'}
+class2={'sunil','gihan','manik'}
+comman=class1.intersection(class2) #ඡේදනය
+print(comman)
+diff=class1.difference(class2)# class1 ගේ පමනක් ඇති
+print(diff)
+union= class1.union(class2) # class1 & class 2 දෙකේම සිටින
+print(union)
+
+
+
+
